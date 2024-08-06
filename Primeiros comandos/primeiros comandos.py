e639b6acdcd52eca6983af6d8c9e3942a0a41a6f
André <andreluizazevedo56@gmail.com>
2024-08-06 15:29:29 -0300

    first commit

---
+++ Primeiros comandos/primeiros comandos.py
@@ -0,0 +1,18 @@
+# Escreva Olá Mundo - OBS: as mensagens devem estar entre aspas sempre"
+print ("Olá, Mundo!")
+# Represente soma de números - OBS: equação e números não necessitam ficar entre aspas
+print (7 + 4)
+#junte os números
+print ("7" + "4")
+#junte mensagem com número - OBS: teria quer separar em vírgulas
+print ("olá, 5")
+#represente em variáveis sua informação e depois faça aparecer na tela - toda variável é um objeto que recebem valores
+nome = "André"
+idade = 20
+peso = 70
+print (nome,idade,peso)
+#faça agora a interatividade com o usuário as informações básicas - obs: tem que usar o input
+nome = input ("Qual é seu nome?")
+idade = input ("Quantos anos você tem?")
+peso = input ("Quanto você pesa?")
+print (nome,idade,peso)
---
+++ Primeiros comandos/primeitos comandos desafio.py
@@ -0,0 +1,9 @@
+#DESAFIO 1 - Crie um script python que leia o nome de uma pessoa e mostra mensagem de boas-vindas de acordo com o valor digitado
+nome = input("Olá, qual seu nome?")
+print (f"Prazer te conhecer, {nome}!")
+#DESAFIO 2 - Crie um script Python que leia o dia, o mês e o ano de nascimento de uma pessoa e mostre a mensagem com a data formatada
+dia = input(f"{nome} Qual foi dia que você nasceu?")
+mês = input("Qual o mês que você nasceu? (Escreva em números)")
+ano = input("Qual ano que você nasceu? (Escreva em quatro dígitos)")
+print (f"Esta sua data correta? {dia}/{mês}/{ano}")
+#DESAFIO 3 -Crie um script Python que leia dois números e tenta mostrar a soma entre eles
