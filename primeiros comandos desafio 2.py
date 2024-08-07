68a761923aa934afed76d8070074c12fc5325f75
André <andreluizazevedo56@gmail.com>
2024-08-06 21:01:58 -0300

    teste

--- primeiros comandos desafio 2.py
+++ primeiros comandos desafio 2.py
@@ -1,3 +1,5 @@
-peso = input("qual é seu peso?")
-altura = input("qual é a sua altura?")
-print (peso,altura) #LEMBRE-SE QUE O PRINT NÃO SE ESCREVE COM SINAL DE IGUAL
+#peso = input("qual é seu peso?")
+#altura = input("qual é a sua altura?")
+#print (peso,altura) #LEMBRE-SE QUE O PRINT NÃO SE ESCREVE COM SINAL DE IGUAL
+
+print ("Olá, mundo!")
