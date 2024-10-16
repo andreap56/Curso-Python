bde97c3475a527edffb641dbbeb4a02d6a01d137
André <andreluizazevedo56@gmail.com>
2024-10-16 16:32:00 -0300

    aula

---
+++ Condicional simples e composta/condicional simples.py
@@ -0,0 +1,9 @@
+#Leia dois valores inteiros e compara ambos
+
+x = int(input("Digite um valor inteiro: "))
+y = int(input("Digite um outro valor inteiro: "))
+
+if (x>y): 
+    print("O primeiro valor é maior que o segundo! ")
+if (x<y):
+    print("O segundo valor é maior que o primeiro! ")
