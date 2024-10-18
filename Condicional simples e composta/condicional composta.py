fa34082790838d67dc6e42ce44938a57063ccfd7
André <andreluizazevedo56@gmail.com>
2024-10-18 13:16:09 -0300

    laço de repetição

---
+++ Condicional simples e composta/condicional composta.py
@@ -0,0 +1,11 @@
+#Leia dois valores inteiros e compara ambos
+
+x = int(input("Escreva o primeiro valor inteiro: "))
+y = int(input("Escreva o segundo valor inteiro: "))
+
+if (x>y):
+    print ("O primeiro número é maior que o segundo! ")
+if (x<y):
+    print ("O segundo valor é maior que o primeiro! ")
+else:
+    print ("Ambos os números são iguais! ")
---
+++ While e for - repetição e laço/while e for.py
@@ -0,0 +1,11 @@
+#While
+
+x = 1
+while (x<6):
+    x = x + 2
+    print(x)
+
+# For
+
+for a in range (1,6,2):
+  print(a)
